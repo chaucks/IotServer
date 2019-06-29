96a352b7391a6f3df6770575f947e1a26e0d1233
chaucks <chauckslee@gmail.com>
2019-06-29 11:15:20 +0800

    optimization.

--- app/src/main/java/com/xcoder/iotserver/utensil/Io.java
+++ app/src/main/java/com/xcoder/iotserver/utensil/Io.java
@@ -98,9 +98,9 @@ public class Io {
      * @throws IOException          IOException
      * @throws InterruptedException InterruptedException
      */
-    public static void read(final InputStream is, final int available, final int length, final Ir ir) throws IOException {
+    public static void read(final InputStream is, final int available, int length, final Ir ir) throws IOException {
         if (length > available) {
-            throw new RuntimeException("length greater than available...");
+            length = available;
         }
         for (int i = 0, j = available / length; i < j; i++) {
             byte[] buffer = new byte[length];
